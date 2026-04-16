9dcafaade7c703e48bc4a74ecb61654fee9129aa
diegoaguiar968-dotcom <diego.aguiar968@al.unieduk.com.br>
2026-04-16 04:18:25 -0300

    Delete routes directory

--- routes/minuta.js
+++
@@ -1,8 +0,0 @@
-// routes/minuta.js
-const router = require('express').Router();
-const { gerarMinutaHandler } = require('../controllers/minutaController');
-
-router.post('/generate', gerarMinutaHandler);
-router.get('/generate', gerarMinutaHandler); // compatibilidade com frontend
-
-module.exports = router;
