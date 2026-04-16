990e75c1e27343421239295cde64b20115810f0e
diegoaguiar968-dotcom <diego.aguiar968@al.unieduk.com.br>
2026-04-16 04:18:17 -0300

    Delete middleware directory

--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-/**
- * middleware/logger.js
- */
-function requestLogger(req, _res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-}
-
-module.exports = { requestLogger };
